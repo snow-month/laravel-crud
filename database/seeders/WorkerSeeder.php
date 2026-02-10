6d2e07287ae5e2fb9c1f54d0440c4cd2b2d79297
snow-month <johndoe@example.com>
2026-02-10 04:45:53 +0300

    добавил WorkerFactory

--- database/seeders/WorkerSeeder.php
+++ database/seeders/WorkerSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Worker;
+use Database\Factories\WorkerFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,10 +14,12 @@ class WorkerSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('workers')->insert([
-            ['name' => 'name1', 'surname' => 'surname1'],
-            ['name' => 'name2', 'surname' => 'surname2'],
-            ['name' => 'name3', 'surname' => 'surname3'],
-        ]);
+//        DB::table('workers')->insert([
+//            ['name' => 'name1', 'surname' => 'surname1'],
+//            ['name' => 'name2', 'surname' => 'surname2'],
+//            ['name' => 'name3', 'surname' => 'surname3'],
+//        ]);
+
+        Worker::factory(10)->create();
     }
 }
